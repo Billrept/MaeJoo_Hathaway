1f2575741d18413e39924f28376cbbd4d2acf8c0
noflnofl <phurin.byp@gmail.com>
2024-10-04 20:57:53 +0700

    removed unecessary files

--- app/model_handler.py
+++
@@ -1,7 +0,0 @@
-from models import arima_prediction, garch_prediction
-
-# Predict stock price and volatility for the next day
-def predict_next_day_price_and_volatility(ticker, stock_data):
-    predicted_price = arima_prediction(stock_data)
-    predicted_volatility = garch_prediction(stock_data)
-    return predicted_price, predicted_volatility
